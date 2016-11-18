0e27236ddec0edae84dd8f2915a8e01f5abfe17f
夏幸楠 <xingnan.xia@vanderbilt.edu>
2016-11-18 13:44:12 -0600

    helloworld

---
+++ hello/js/App.js
@@ -0,0 +1,34 @@
+//Javascript document 
+
+function getData(pageName){
+
+	//send the name over to the php file.
+	//This is the jquery post method
+	var posting = $.post("php/script.php", {
+	
+		page_name: pageName
+	
+	});
+	
+	//what will happen when the posting is done.
+	posting.done(function(data){
+		
+		//jQuery 
+		$("#content").html(data);
+	
+	});
+
+	//what happens if it fails.
+	posting.fail(function(){
+	
+		alert("failed");
+	
+	});
+}
+
+//When the web page is loaded. 
+$(document).ready(function(){
+
+	getData("about");
+
+});
